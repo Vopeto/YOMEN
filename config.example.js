bf7b0e21a30941bb8d82c4f4f59c754b2f42b854
Vopeto <giacomo.g09@outlook.it>
2024-03-29 13:54:37 +0100

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,12 +1,12 @@
 module.exports = {
   keywords: ["mr beast", "pass"],
   comments: ["hello","let's be a friend","subs to subs ahaha"],
-  usernamegoogle: "username_google/enuae16@gmail.com",
+  usernamegoogle: "enuae16/enuae16@gmail.com",
   passwordgoogle: "Diocaneladro22",
   trending: false, // set true if you want comment on trending videos or false if you want comment on search videos
   newVideos: false, // set true if you want comment on new videos or false if you want comment on top videos
   copycomment:true,  // set tru if you want copy comment from array or false if you want random comment
   userdatadir : "fdciabdul",  // set your userdatadir
-  ai : false,  //set true if you want use ai comment or false if you want use random comment or copy comment
+  ai : true,  //set true if you want use ai comment or false if you want use random comment or copy comment
   apiKey : "",  // set your api key from 
 };
